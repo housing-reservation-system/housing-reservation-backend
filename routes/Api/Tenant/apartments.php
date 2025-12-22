a840b4111274c7ad860441bd439a42597f6fdc8e
shemaaaloth005 <shemaaaloth005@gmail.com>
2025-12-22 17:38:13 +0300

    commit

--- routes/Api/Tenant/apartments.php
+++ routes/Api/Tenant/apartments.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Tenant\ApartmentController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('tenant')->name('tenant')->group(function(){
 Route::get('apartments',[ApartmentController::class,'index']);
-Route::get('apartments',[ApartmentController::class,'show']);
+Route::get('apartments/{apartment}',[ApartmentController::class,'show']);
 });
